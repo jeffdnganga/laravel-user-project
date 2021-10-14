ba00ace09c35f253fc8d674f778fadf0c625e11f
jeffdnganga <jeffdaniels950@gmail.com>
2021-10-15 00:41:18 +0300

    Completed user profile assignment project with sign in, sign up and profile update

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('frontend.index');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/my-profile', [App\Http\Controllers\Frontend\UserController::class, 'myProfile']);
+Route::post('/my-profile-update', [App\Http\Controllers\Frontend\UserController::class, 'profileUpdate']);
+
+Route::get('logout', [App\Http\Controllers\Auth\LoginController::class, 'logout']);
